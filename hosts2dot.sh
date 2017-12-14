d62286878be9ab6ede84cadc03d4dc54454c0d73
deanturpin <git@lvbtn.uk>
2017-12-14 22:08:37 +0000

    Print the line as node name

--- hosts2dot.sh
+++ hosts2dot.sh
@@ -9,30 +9,31 @@ soy_yo [label=$(hostname)]
 graph [label="$0 - $(date)" labelloc=top labeljust=left]
 !
 
-# Trace some routes and generate the dot links
+# Parse each line of the hosts file passed on stdin
 while read line; do
 
+  # Extract the first host or IP on the line
   host=${line/\ *}
-  comment=${line/*\#}
-  full_name=\"$line\"
+
+  # Surround the complete line with quotes so it prints nicely
+  line=\"$line\"
 
   # Is the host responding?
   ping -w 1 -c 1 $host >& /dev/null && color=green || color=red
-  echo $full_name [fillcolor=$color fontcolor=white]
+  echo $line [fillcolor=$color fontcolor=white]
 
-  # Print the start point (me)
-  echo // $line
+  # Print the origin of the trace
   echo -n soy_yo--
 
   # Print the route
-  tracepath $host | while read line; do
-    [[ ! $line =~ 'no reply' ]] \
-      && [[ $line =~ [[:digit:]]+:[[:space:]]+([[:graph:]]+) ]] \
+  tracepath $host | while read hop; do
+    [[ ! $hop =~ 'no reply' ]] \
+      && [[ $hop =~ [[:digit:]]+:[[:space:]]+([[:graph:]]+) ]] \
         && echo -n \"${BASH_REMATCH[1]}\"--
   done
 
   # Print the end point
-  echo $full_name
+  echo $line
 done
 
 # Complete the dot file
